83fb2709cd133638e54d49ed8c2aa68630961723
Pandorre <46975404+Pandorre@users.noreply.github.com>
2019-01-23 20:50:57 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import datetime
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'title' : 'HELLO!',
+      'time': timeString
+      }
+   return render_template('index.html', **templateData)
+
+@app.route('/cakes')
+def cakes():
+    return render_template('cakes.html')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
